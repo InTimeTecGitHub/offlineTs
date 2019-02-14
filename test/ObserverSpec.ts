77a11a3cac9b495445c268590f82c271ffc8ef3e
Nandita-Khemnani <47440523+Nandita-Khemnani@users.noreply.github.com>
2019-02-14 17:23:57 +0530

    Test cases observer (#3)
    Unit tests for observer and gulp tasks.

--- test/ObserverSpec.ts
+++ test/ObserverSpec.ts
@@ -1,24 +1,65 @@
 import {expect} from "chai";
 import {SampleObserver} from "./fixture/SampleObserver";
 import {ServiceStatus} from "../src/ServiceStatus";
-describe("@Observer", () => {
-    it("should update state of observer when state of subject changes", () => {
+import {SampleObserverOne} from "./fixture/SamleObserverOne";
+
+describe("@ObserverStatusForOneClass", () => {
+    before(() => {
+        ServiceStatus.cancelInterval();
+    });
+
+    it("should update the annotated class when service status changes", () => {
         let observer = new SampleObserver();
+        observer.state = 0;
         let initial = observer.state;
 
-        ServiceStatus.instance.goOnline();
+        ServiceStatus.goOnline();
         let result = observer.state;
-        ServiceStatus.instance.goOffline();
+        ServiceStatus.goOffline();
 
-        expect(initial).to.be.equal(0);
         expect(result).to.be.equal(999);
-
-        ServiceStatus.instance.cancelInterval();
     });
 
     it("should set default value of sample observer as 0", () => {
         let observer = new SampleObserver();
         expect(observer.state).to.be.equal(0);
-        ServiceStatus.instance.cancelInterval();
     });
+});
+
+describe("@ObserverStatusForAllClasses", () => {
+    before(() => {
+        ServiceStatus.cancelInterval();
+    });
+    it("should update all annotated classes when service status changes - Two observers from same class", () => {
+        let observerOne = new SampleObserver();
+        observerOne.state = 0;
+
+        let observerTwo = new SampleObserver();
+        observerTwo.state = 100;
+
+        let initialStateOne = observerOne.state;
+        let initialStateTwo = observerTwo.state;
+
+        ServiceStatus.goOnline();
+        let resultObserverOne = observerOne.state;
+        let resultObserverTwo = observerTwo.state;
+        ServiceStatus.goOffline();
+
+        expect(resultObserverOne).to.be.equal(999);
+        expect(resultObserverTwo).to.be.equal(999);
+    });
+
+    it("should update all annotated classes when service status changes - observer from a different class", () => {
+        let newObserver = new SampleObserverOne();
+        newObserver.state = 0;
+
+        let initialState = newObserver.state;
+
+        ServiceStatus.goOnline();
+        let resultingState = newObserver.state;
+        ServiceStatus.goOffline();
+
+        expect(resultingState).to.be.equal(99);
+    });
+
 });
