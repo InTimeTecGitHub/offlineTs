c5494df3db9e4dc4b72944e4669615f2ffb4016a
sasidakh <akhilsasidharan1991@gmail.com>
2019-02-20 16:11:03 +0530

    adding a single test file to run the tests from

--- test/PingSpec.ts
+++
@@ -1,7 +0,0 @@
-// import {expect, use} from "chai";
-// import {Ping} from "../src/Ping";
-// describe("@Ping", () => {
-//     it("should replace ping with custom function", () => {
-//         throw new Error();
-//     });
-// });
