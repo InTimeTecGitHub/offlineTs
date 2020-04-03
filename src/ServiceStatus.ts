54059d146c9b45722ef3cd90163b8776e5029b32
harshith <harshith.gowda@intimetec.com>
2020-04-03 16:42:55 +0530

    Added optional API parameter to startPing function

--- src/ServiceStatus.ts
+++ src/ServiceStatus.ts
@@ -9,6 +9,7 @@ export enum StateType {
 
 export class ServiceStatus {
     private period?: number;
+    private pingApi: string;
     private interval: any;
     private state: StateType;
     private observers: Map<number, Observer> = new Map<number, any>();
@@ -45,6 +46,27 @@ export class ServiceStatus {
         return this.response || new Response();
     }
 
+    private get PingApi(): string {
+        return this.pingApi + "?_=" + new Date().getTime()
+    }
+
+    private set PingApi(api: string) {
+        this.pingApi = api;
+    }
+
+    private get Init(): RequestInit {
+        return {
+            method: "HEAD",
+            headers: {
+                "Accept": "application/json",
+                "Content-Type": "application/json"
+            },
+            mode: "cors",
+            cache: "default",
+            credentials: "same-origin"
+        };
+    }
+
     goOnline() {
         if (this.State !== StateType.ONLINE) {
             this.State = StateType.ONLINE;
@@ -65,7 +87,7 @@ export class ServiceStatus {
 
     private async callback() {
         if (!this.ping) return;
-        await this.ping.ping()
+        await this.ping.ping(this.PingApi, this.Init)
             .then(async (response: Response) => {
                 this.response = response;
                 if (this.response.status === 200) return this.goOnline();
@@ -80,9 +102,14 @@ export class ServiceStatus {
         this.interval && (clearInterval(this.interval));
     }
 
-    startPing(period: number) {
+    startPing(period: number, api?: string) {
         if (!period) throw new Error("Cannot start ping without interval time.");
         if (!this.ping) throw new Error("No Ping service set.");
+        if (api) {
+            this.PingApi = api;
+        } else {
+            this.PingApi = "/favicon.ico"
+        }
         this.Period = period;
     }
 
